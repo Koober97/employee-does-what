0bcf23a16b8c21e45b375eaea47d0b8357bd5026
Koober97 <koober@Brians-MacBook-Pro.local>
2022-11-12 18:43:35 -0500

    created first set of array questions

--- index.js
+++ index.js
@@ -52,9 +52,79 @@ function deleteEmployee() {
 
 // Query database
 function showEmployees() {
-  db.query("SELECT * FROM employee", function (err, results) {
-    console.table(results);
-    showOptions();
+  db.query(
+    `SELECT employee.id,employee.first_name,employee.last_name,CONCAT(manager.first_name," ", manager.last_name) AS managerName,role.title,role.salary,department.name AS department  
+  FROM employee employee LEFT JOIN employee manager ON employee.manager_id=manager.id
+ LEFT JOIN role ON employee.role_id = role.id
+LEFT JOIN department ON role.department_id = department.id;`,
+    function (err, results) {
+      console.table(results);
+      showOptions();
+    }
+  );
+}
+
+// Add a new employee
+function addEmployee() {
+  let managerChoices = [];
+  let roleChoices = [];
+  // Creates an array of role choices for the first question
+  db.query(`SELECT * FROM role`, function (err, results) {
+    roleChoices = results.map(({ id, title}) => {
+      return {
+        value: id,
+        name: title,
+      };
+    });
+    // Creates an array of all employees to select an employee's manager in the last question
+    db.query(`SELECT * FROM employee`, (err, results) => {
+      managerChoices = results.map(({ id, first_name, last_name}) => {
+        return { name: `${first_name} ${last_name}`, value: id};
+      });
+      managerChoices.push({ name: "none", value: null });
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "role",
+            message: "What is the employee's role?",
+            choices: roleChoices,
+          },
+          {
+            type: "input",
+            name: "first_name",
+            message: "Enter employee's first name:",
+          },
+          {
+            type: "input",
+            name: "last_name",
+            message: "Enter employee's last name:",
+          },
+          {
+            type: "list",
+            name: "manager",
+            message: "Who is the employee's manager?",
+            choices: managerChoices,
+          },
+        ])
+        .then((answers) => {
+          const sql = `INSERT INTO employee(first_name,last_name,role_id,manager_id) VALUES (?,?,?,?)`;
+          const params = [
+            answers.first_name,
+            answers.last_name,
+            answers.role,
+            answers.manager,
+          ];
+          db.query(sql, params, (err, result) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            console.log(result);
+            showOptions();
+          });
+        });
+    });  
   });
 }
 
